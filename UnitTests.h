b064dad310ae05d4caa5a8c1de53e95e9c2efba5
zhoskiy <45569696+zhoskiy@users.noreply.github.com>
2020-11-19 17:33:58 +0300

    Initial commit

---
+++ UnitTests.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef UNIT_TESTS_H
+#define UNIT_TESTS_H
+
+#include "MemoryManager.h"
+
+// mock_mmem frees mmemory and segment table and assigns new structures for them.
+void make_mmem(MEMORY*);
+
+#endif
